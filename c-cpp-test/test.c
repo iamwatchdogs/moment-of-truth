6bb07ff9f0e9f9bba4ac346da2f4b2cd880530eb
Shamith <shamith301102@gmail.com>
2023-10-11 19:55:26 +0530

    Update test.c

--- c-cpp-test/test.c
+++ c-cpp-test/test.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
+int main() {
   int len = strlen("Hello World !!!...");
   printf("length of \"Hello World !!!...\" is %d",len);
   return 0;
