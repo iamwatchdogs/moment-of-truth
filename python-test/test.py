a7a7c9ecf300710fbc255d7abba7b813bd88802b
Shamith <shamith301102@gmail.com>
2023-10-11 22:45:52 +0530

    Update test.py

--- python-test/test.py
+++ python-test/test.py
@@ -1,12 +1,32 @@
-lower = 900
-upper = 1000
+# Python program for the above approach
 
-print("Prime numbers between", lower, "and", upper, "are:")
+# Function to reverse the words in string
+def reverse_word(s, start, end):
+	while start < end:
+		s[start], s[end] = s[end], s[start]
+		start += 1
+		end -= 1
 
-for num in range(lower, upper + 1):
-   if num > 1:
-       for i in range(2, num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num)
+# Function to reverse the string
+def reverse_string(s):
+	s = list(s)
+	start, end = 0, len(s) - 1
+	reverse_word(s, start, end)
+
+	start = end = 0
+
+	# Iterate over the string S
+	while end < len(s):
+		if s[end] == ' ':
+			reverse_word(s, start, end - 1)
+			start = end + 1
+		end += 1
+
+	# Reverse the words
+	reverse_word(s, start, end - 1)
+	return ''.join(s)
+
+
+# Driver Code
+S = "geeks quiz practice code"
+print(reverse_string(S))
