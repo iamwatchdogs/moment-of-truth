f9b0eb99c71489b4a53238048ac1fa4e30965d52
Shamith <shamith301102@gmail.com>
2023-10-20 19:04:12 +0530

    Delete gonna-code-in-ruby/test.rb

--- gonna-code-in-ruby/test.rb
+++
@@ -1,8 +0,0 @@
-# Ruby program to get the length of 
-arr1 = [101,"Rahul",15000];
-arr2 = [101,102,103,104,105];
-arr3 = Array.new(10);
-
-print "Length of Array1: ",arr1.length(),"\n";
-print "Length of Array2: ",arr2.length(),"\n";
-print "Length of Array3: ",arr3.length(),"\n";
