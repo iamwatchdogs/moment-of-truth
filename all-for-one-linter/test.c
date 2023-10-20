bd07ed10f426ad73ff759c0e9edef3eecc916ee3
Shamith <shamith301102@gmail.com>
2023-10-20 12:50:42 +0530

    Update test.c

--- all-for-one-linter/test.c
+++ all-for-one-linter/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main() {
     int base, exp;
     long double result = 1.0;
@@ -6,7 +7,6 @@ int main() {
     scanf("%d", &base);
     printf("Enter an exponent: ");
     scanf("%d", &exp);
-
     while (exp != 0) {
         result *= base;
         --exp;
